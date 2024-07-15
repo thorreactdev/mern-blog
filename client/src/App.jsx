5d5b5db0fcddfda40a69dd91557e613cd57d8340
thorreactdev <prajapativinay140404@gmail.com>
2024-07-15 20:06:32 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+import About from "./Pages/About";
+import Projects from "./Pages/Projects";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
-  
   return(
-    <div className="text-7xl">
-      helo
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/project" element={<Projects/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </Router>
   )
 }
 
